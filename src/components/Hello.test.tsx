9c4596508bc2898cb30993321ec309a614e98b40
Jacob Tomaw <jtomaw@shoprunner.com>
2019-03-06 15:46:30 -0500

    Tests require adapter

---
+++ src/components/Hello.test.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import * as enzyme from 'enzyme';
+import Hello from './Hello';
+import Adapter from 'enzyme-adapter-react-16';
+enzyme.configure({ adapter: new Adapter() });
+it('renders the correct text when no enthusiasm level is given', () => {
+  const hello = enzyme.shallow(<Hello name='Daniel' />);
+  expect(hello.find(".greeting").text()).toEqual('Hello Daniel!')
+});
